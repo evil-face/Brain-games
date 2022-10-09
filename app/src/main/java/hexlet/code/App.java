12bd30b4272238fc75b3853d8ea63f8e4884a5d9
evil_face <den.bragin@gmail.com>
2022-10-09 15:54:50 +0600

    Update App.java with menu selection

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
 package hexlet.code;
 
+import hexlet.code.games.Even;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.welcome();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1 -> Cli.showGreeting();
+            case 2 -> Even.runGame();
+            default -> System.out.println("See you!");
+        }
     }
 }
