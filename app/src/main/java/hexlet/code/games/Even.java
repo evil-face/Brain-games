704f63c19d52ab53c3c38d0f983193d1d1e9b699
evil_face <den.bragin@gmail.com>
2022-10-09 19:52:33 +0600

    Fix Even.java to work with new Engine.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -2,37 +2,19 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 import java.util.Random;
-import java.util.Scanner;
-
 public class Even {
-    public static void runGame() {
-        Scanner scanner = new Scanner(System.in);
+    public static void startEvenGame() {
         Random rand = new Random();
-        String name = Engine.getName();
-        int numOfRightAnswers = 0;
+        String[] questions = new String[Engine.ROUNDS];
+        String[] answers = new String[Engine.ROUNDS];
+        String task = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-
-        while (numOfRightAnswers < 3) {
+        for (int i = 0; i < Engine.ROUNDS; i++) {
             int randNumber = rand.nextInt(100);
-            boolean isEven = randNumber % 2 == 0;
-            String correctAnswer = (randNumber % 2 == 0) ? "yes" : "no";
-
-            System.out.println("Question: " + randNumber);
-            System.out.print("Your answer: ");
-            String userAnswer = scanner.next();
-            boolean isEvenUserAnswer = userAnswer.equals("yes");
-
-            if (isEvenUserAnswer == isEven) {
-                System.out.println("Correct!");
-                numOfRightAnswers++;
-            } else {
-                System.out.print("'" + userAnswer + "'" + " is wrong answer ;(. ");
-                System.out.println("Correct answer was " + "'" + correctAnswer + "'");
-                System.out.println("Let's try again, " + name + "!");
-                return;
-            }
+            questions[i] = String.valueOf(randNumber);
+            answers[i] = (randNumber % 2 == 0) ? "yes" : "no";
         }
-        System.out.println("Congratulations, " + name + "!\n");
+
+        Engine.runGame(task, questions, answers);
     }
 }
