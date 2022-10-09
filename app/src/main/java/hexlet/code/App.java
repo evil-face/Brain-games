c1a985be277bac2a280cde896688f30e28032609
evil_face <den.bragin@gmail.com>
2022-10-09 19:51:21 +0600

    Add new game in the menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
 import hexlet.code.games.Even;
+import hexlet.code.games.Calc;
 import java.util.Scanner;
 
 public class App {
@@ -9,13 +10,15 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         int choice = scanner.nextInt();
 
         switch (choice) {
             case 1 -> Cli.showGreeting();
-            case 2 -> Even.runGame();
+            case 2 -> Even.startEvenGame();
+            case 3 -> Calc.startCalcGame();
             default -> System.out.println("See you!");
         }
     }
