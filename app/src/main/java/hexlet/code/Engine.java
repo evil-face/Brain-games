0c7e79daf89073a75a5d15960439929deff5d58d
evil_face <den.bragin@gmail.com>
2022-10-09 19:52:10 +0600

    Update Engine.java with new logic

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import java.util.Scanner;
 public class Engine {
+    public static final int ROUNDS = 3;
     public static String getName() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("\nWelcome to the Brain Games!");
@@ -10,4 +11,27 @@ public class Engine {
         System.out.println("Hello, " + name + "!");
         return name;
     }
+
+    public static void runGame(String task, String[] questions, String[] answers) {
+        String name = getName();
+        System.out.println(task);
+
+        for (int i = 0; i < ROUNDS; i++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Question: " + questions[i]);
+            System.out.print("Your answer: ");
+            String userAnswer = scanner.next();
+
+            if (userAnswer.equals(answers[i])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.print("'" + userAnswer + "'" + " is wrong answer ;(. ");
+                System.out.println("Correct answer was " + "'" + answers[i] + "'");
+                System.out.println("Let's try again, " + name + "!");
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + name + "!\n");
+    }
 }
+
