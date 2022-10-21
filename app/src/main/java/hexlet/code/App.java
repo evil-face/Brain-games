00e24311caf4d7e7c774c1609ea21d3769c40e1a
evil_face <den.bragin@gmail.com>
2022-10-21 21:12:26 +0600

    Fix App.java to not use magic numbers in switch expression

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -19,15 +19,15 @@ public class App {
         System.out.println("6 - Prime");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
-        int choice = scanner.nextInt();
+        String choice = scanner.next();
 
         switch (choice) {
-            case 1 -> Cli.showGreeting();
-            case 2 -> Even.startEvenGame();
-            case 3 -> Calc.startCalcGame();
-            case 4 -> GCD.startGcdGame();
-            case 5 -> Progression.startProgGame();
-            case 6 -> Prime.startPrimeGame();
+            case "1" -> Cli.showGreeting();
+            case "2" -> Even.startEvenGame();
+            case "3" -> Calc.startCalcGame();
+            case "4" -> GCD.startGcdGame();
+            case "5" -> Progression.startProgGame();
+            case "6" -> Prime.startPrimeGame();
             default -> System.out.println("See you!");
         }
     }
