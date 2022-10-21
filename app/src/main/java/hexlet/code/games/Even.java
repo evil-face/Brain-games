189d87fabd50a686cce3fe4bb90fd9b8a55e45e1
evil_face <den.bragin@gmail.com>
2022-10-21 20:35:00 +0600

    Update Even.java with code comments

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -3,18 +3,22 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 import java.util.Random;
 public class Even {
+
+    // main method to run the game
     public static void startEvenGame() {
         Random rand = new Random();
         String[] questions = new String[Engine.ROUNDS];
         String[] answers = new String[Engine.ROUNDS];
         String task = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
+        // generate questions and answers array
         for (int i = 0; i < Engine.ROUNDS; i++) {
             int randNumber = rand.nextInt(100);
             questions[i] = String.valueOf(randNumber);
             answers[i] = (randNumber % 2 == 0) ? "yes" : "no";
         }
 
+        // call the engine with game parameters
         Engine.runGame(task, questions, answers);
     }
 }
