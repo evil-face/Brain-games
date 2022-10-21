da261c416f0be810f54ee00e3c7a0568b7623b6c
evil_face <den.bragin@gmail.com>
2022-10-21 20:24:05 +0600

    Update App.java with new menu item (Prime game)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,7 @@ import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.GCD;
 import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
 import java.util.Scanner;
 
 public class App {
@@ -15,6 +16,7 @@ public class App {
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         int choice = scanner.nextInt();
@@ -25,6 +27,7 @@ public class App {
             case 3 -> Calc.startCalcGame();
             case 4 -> GCD.startGcdGame();
             case 5 -> Progression.startProgGame();
+            case 6 -> Prime.startPrimeGame();
             default -> System.out.println("See you!");
         }
     }
