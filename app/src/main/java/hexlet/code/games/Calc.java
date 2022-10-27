c8e37c0227e20e1c7bfd7b6238a9bd19ef22f944
evil_face <den.bragin@gmail.com>
2022-10-27 14:18:07 +0600

    Update Calc.java with separate method to randomly pick math operation

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -6,38 +6,43 @@ import java.util.Random;
 public class Calc {
 
     // number of different math operations used in game
-    public static final int TYPE = 3;
+    private static final int TYPE = 3;
+    private static String[] questions = new String[Engine.ROUNDS];
+    private static String[] answers = new String[Engine.ROUNDS];
 
     // main method to run the game
     public static void startCalcGame() {
-        Random rand = new Random();
-        String[] questions = new String[Engine.ROUNDS];
-        String[] answers = new String[Engine.ROUNDS];
         String task = "What is the result of the expression?";
 
         // generate questions and answers array
         for (int i = 0; i < Engine.ROUNDS; i++) {
-            int firstRandNumber = rand.nextInt(Engine.RANGE);
-            int secondRandNumber = rand.nextInt(Engine.RANGE);
-            int type = rand.nextInt(TYPE);
-
-            switch (type) {
-                case 0:
-                    questions[i] = firstRandNumber + " + " + secondRandNumber;
-                    answers[i] = String.valueOf(firstRandNumber + secondRandNumber);
-                    break;
-                case 1:
-                    questions[i] = firstRandNumber + " - " + secondRandNumber;
-                    answers[i] = String.valueOf(firstRandNumber - secondRandNumber);
-                    break;
-                default:
-                    questions[i] = firstRandNumber + " * " + secondRandNumber;
-                    answers[i] = String.valueOf(firstRandNumber * secondRandNumber);
-                    break;
-            }
+            getRandomQuestionAndAnswer(i);
         }
 
         // call the engine with game parameters
         Engine.runGame(task, questions, answers);
+    }
+
+    // randomly pick math operation and generate question and answer
+    private static void getRandomQuestionAndAnswer(int questionNum) {
+        Random rand = new Random();
+        int firstRandNumber = rand.nextInt(Engine.RANGE);
+        int secondRandNumber = rand.nextInt(Engine.RANGE);
+        int type = rand.nextInt(TYPE);
+
+        switch (type) {
+            case 0:
+                questions[questionNum] = firstRandNumber + " + " + secondRandNumber;
+                answers[questionNum] = String.valueOf(firstRandNumber + secondRandNumber);
+                break;
+            case 1:
+                questions[questionNum] = firstRandNumber + " - " + secondRandNumber;
+                answers[questionNum] = String.valueOf(firstRandNumber - secondRandNumber);
+                break;
+            default:
+                questions[questionNum] = firstRandNumber + " * " + secondRandNumber;
+                answers[questionNum] = String.valueOf(firstRandNumber * secondRandNumber);
+                break;
+        }
     }
 }
