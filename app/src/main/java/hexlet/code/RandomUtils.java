67a24c6bf5ec7af1b526c8f1573a3fbde1175425
evil_face <den.bragin@gmail.com>
2022-10-30 20:32:03 +0600

    feat: Add RandomUtils.java
    To use different methods to work with random numbers

---
+++ app/src/main/java/hexlet/code/RandomUtils.java
@@ -0,0 +1,24 @@
+package hexlet.code;
+
+import java.util.Random;
+
+public class RandomUtils {
+
+    // simple method to generate one random number of max length
+    public static int getRandomNumber(int max) {
+        Random rand = new Random();
+        return rand.nextInt(max);
+    }
+
+    // a method to generate non-zero step for progression
+    public static int generateStep(int diff) {
+        Random rand = new Random();
+        int step = 0;
+
+        // change diff value in case it's generated as zero to make game more fun
+        while (step == 0) {
+            step = rand.nextInt(diff);
+        }
+        return step;
+    }
+}
