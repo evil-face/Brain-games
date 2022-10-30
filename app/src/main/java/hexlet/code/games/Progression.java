4d9480a08bf207623e42955f4b9de9fb4541e4ac
evil_face <den.bragin@gmail.com>
2022-10-30 20:28:37 +0600

    refactor: Add RandomUtils.java support (remove generateStep() method)

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,9 +1,12 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-import java.util.Random;
+import hexlet.code.RandomUtils;
 
 public class Progression {
+
+    // game task
+    private static final String TASK = "What number is missing in the progression?";
 
     // length of generated progression
     private static final int LENGTH = 10;
@@ -13,16 +16,14 @@ public class Progression {
 
     // main method to run the game
     public static void startProgGame() {
-        Random rand = new Random();
         String[] questions = new String[Engine.ROUNDS];
         String[] answers = new String[Engine.ROUNDS];
-        String task = "What number is missing in the progression?";
 
         // generate questions and answers array
         for (int i = 0; i < Engine.ROUNDS; i++) {
-            int randPlace = rand.nextInt(LENGTH);
-            int init = rand.nextInt(Engine.RANGE);
-            int step = generateStep();
+            int randPlace = RandomUtils.getRandomNumber(LENGTH);
+            int init = RandomUtils.getRandomNumber(Engine.RANGE);
+            int step = RandomUtils.generateStep(DIFF);
             // get new array
             String[] currentStr = generateProg(init, step, LENGTH);
 
@@ -34,19 +35,7 @@ public class Progression {
         }
 
         // call the engine with game parameters
-        Engine.runGame(task, questions, answers);
-    }
-
-    // a method to generate non-zero step for progression
-    private static int generateStep() {
-        Random rand = new Random();
-        int step = 0;
-
-        // change diff value in case it's generated as zero to make game more fun
-        while (step == 0) {
-            step = rand.nextInt(DIFF);
-        }
-        return step;
+        Engine.runGame(TASK, questions, answers);
     }
 
     // a method to generate full progression in string array
