329cf7aa1126877ca720cfabc15784445e64b312
evil_face <den.bragin@gmail.com>
2022-10-30 20:27:08 +0600

    refactor: Add RandomUtils.java support

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,25 +1,27 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-import java.util.Random;
+import hexlet.code.RandomUtils;
+
 public class Even {
+
+    // game task
+    private static final String TASK = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
     // main method to run the game
     public static void startEvenGame() {
-        Random rand = new Random();
         String[] questions = new String[Engine.ROUNDS];
         String[] answers = new String[Engine.ROUNDS];
-        String task = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
         // generate questions and answers array
         for (int i = 0; i < Engine.ROUNDS; i++) {
-            int randNumber = rand.nextInt(Engine.RANGE);
+            int randNumber = RandomUtils.getRandomNumber(Engine.RANGE);
             questions[i] = String.valueOf(randNumber);
             answers[i] = isEven(randNumber) ? "yes" : "no";
         }
 
         // call the engine with game parameters
-        Engine.runGame(task, questions, answers);
+        Engine.runGame(TASK, questions, answers);
     }
 
     // checking if number is even or not
