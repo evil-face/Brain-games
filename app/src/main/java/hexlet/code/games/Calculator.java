49902e2df70cec0bb12275da04b35457b29bf575
evil_face <den.bragin@gmail.com>
2022-11-01 15:07:04 +0600

    refactor: Add TYPES_OF_MATH_OPERANDS constant and implemented its use

--- app/src/main/java/hexlet/code/games/Calculator.java
+++ app/src/main/java/hexlet/code/games/Calculator.java
@@ -6,7 +6,9 @@ import hexlet.code.RandomUtils;
 public class Calculator {
 
     // number of different math operations used in game
-    private static final int TYPE = 3;
+    private static final int COUNT_OF_MATH_OPERANDS = 3;
+
+    private static final String[] TYPES_OF_MATH_OPERANDS = {" + ", " - ", " * "};
 
     // game task
     private static final String TASK = "What is the result of the expression?";
@@ -35,14 +37,14 @@ public class Calculator {
     private static String[] getRandomQuestionAndAnswer() throws RuntimeException {
         int firstRandNumber = RandomUtils.getRandomNumber(Engine.RANGE);
         int secondRandNumber = RandomUtils.getRandomNumber(Engine.RANGE);
-        int typeOfMathOperator = RandomUtils.getRandomNumber(TYPE);
+        int selectedRandomOperand = RandomUtils.getRandomNumber(COUNT_OF_MATH_OPERANDS);
         int answerValue;
         String[] currentPair = new String[2];
 
-        String mathOperator = getStringMathOperator(typeOfMathOperator);
-        currentPair[0] = firstRandNumber + mathOperator + secondRandNumber;
+        String mathOperand = TYPES_OF_MATH_OPERANDS[selectedRandomOperand];
+        currentPair[0] = firstRandNumber + mathOperand + secondRandNumber;
 
-        switch (mathOperator.trim()) {
+        switch (mathOperand.trim()) {
             case "+":
                 answerValue = firstRandNumber + secondRandNumber;
                 break;
@@ -59,14 +61,5 @@ public class Calculator {
         currentPair[1] = String.valueOf(answerValue);
 
         return currentPair;
-    }
-
-    private static String getStringMathOperator(int type) throws RuntimeException {
-        return switch (type) {
-            case 0 -> " + ";
-            case 1 -> " - ";
-            case 2 -> " * ";
-            default -> throw new RuntimeException();
-        };
     }
 }
